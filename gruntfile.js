221bb8009121b77738b31db183717ea02bef93f5
Otávio Guimarães <otaviovgsr@gmail.com>
2017-11-27 23:30:57 -0200

    Adição do bower, grunt e configuração do mesmo

---
+++ gruntfile.js
@@ -0,0 +1,95 @@
+"use strict";
+
+module.exports = function(Grunt)
+{
+    var bowerPath = "./bower_components";
+    var sourcePath = "./src";
+    var distPath = "./dist";
+    var distCssPath = (distPath + "/css");
+    var distJsPath = (distPath + "/js");
+
+    Grunt.initConfig({
+
+        // Concat
+        concat: {
+            options: { separator: "; \n" },
+            vendor: {
+                src: [ 
+                    (bowerPath + "/jquery/dist/jquery.min.js"),
+                    (bowerPath + "/angular/angular.min.js"),
+                ],
+                dest: (distJsPath + "/vendor.js")
+            },
+            app: {
+                src: [ 
+                ],
+                dest: (distJsPath + "/app.js")
+            }
+        },
+
+        // Less
+        less: {
+            options: { compress: true },
+            app: {
+                files: { "./dist/css/app.css": "./src/style.loader.less" }
+            }
+        },
+
+        // Concat CSS
+        concat_css: {
+            vendor: {
+                src: [ 
+                    (bowerPath + "/reset-css/reset.css"),
+                    (bowerPath + "/bootstrap/dist/css/bootstrap.min.css"),
+                ],
+                dest: (distCssPath + "/vendor.css")
+            }
+        },
+
+        // Copy
+        copy: {
+            components_templates: { 
+                expand: true,
+                cwd: (sourcePath + "/components"),
+                src: "**/*.html",
+                dest: (distPath + "/templates/components")
+            },
+            pages_templates: {
+                expand: true,
+                cwd: (sourcePath + "/pages"),
+                src: "**/*.html",
+                dest: (distPath + "/templates/pages")
+            },
+            images: {
+                expand: true,
+                cwd: (sourcePath + "/images"),
+                src: ["**/*.jpg", "**/*.png"],
+                dest: (distPath + "/images")
+            },
+            vendor: {
+                expand: true,
+                cwd: (bowerPath + "/components-font-awesome/fonts"),
+                src: "**",
+                dest: (distPath + "/fonts")
+            }
+        },
+
+        // Watch
+        watch: {
+            options: {},
+            app: {
+                files: (sourcePath + "/**/*"),
+                tasks: ["default"]
+            }
+        }
+    });
+
+    Grunt.loadNpmTasks("grunt-contrib-concat");
+    Grunt.loadNpmTasks("grunt-contrib-less");
+    Grunt.loadNpmTasks("grunt-contrib-copy");
+    Grunt.loadNpmTasks("grunt-contrib-watch");
+    Grunt.loadNpmTasks("grunt-concat-css");
+
+    Grunt.registerTask("default", ["concat:app", "less:app", "copy:components_templates", "copy:pages_templates", "copy:images"]);
+    Grunt.registerTask("vendor", ["concat:vendor", "concat_css:vendor", "copy:vendor"]);
+}
