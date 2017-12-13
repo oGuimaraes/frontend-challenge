4f1f55376ca34fabd7e6f9b9a7bd922719f5b3ac
Otávio Guimarães <otaviovgsr@gmail.com>
2017-12-12 23:15:58 -0200

    Adição dos min e máx das 10 capitais

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,49 @@
-axios.get("https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in (select woeid from geo.places(1) where text='rio de janeiro, rj') and u='c'&format=json")
-.then(function (response) {
-    var forecast = response.data.query.results.channel.item.forecast[0];
-    var high = forecast.high;
-    var low = forecast.low;
 
-    var forecastTable = document.getElementById("forecast-table");
-    var line = document.createElement("tr");
-    forecastTable.appendChild(line);
+for (indice = 0; indice <= 9; indice ++) {
+    var indiceName = 0;
+    var citiesQueries = ['rio de janeiro, rj', 'sao paulo, sp', 'belo horizonte, mg',
+                        'brasilia, go', 'belem, pa', 'salvador, ba', 'curitiba, pr',
+                        'fortaleza, ce', 'manaus, am','joao pessoa, pb']
 
-    var maxColumn = document.createElement("td");
-    var maxTextNode = document.createTextNode(high);
-    line.appendChild(maxColumn);
-    maxColumn.appendChild(maxTextNode);
+    axios.get("https://query.yahooapis.com/v1/public/yql?q=select item from weather.forecast where woeid in (select woeid from geo.places(1) where text=' " + citiesQueries[indice]+ "') and u='c'&format=json")
+    .then(function (response) {
+        var forecast = response.data.query.results.channel.item.forecast[0];
+        var high = forecast.high;
+        var low = forecast.low;
+        
+        var forecastTable = document.getElementById("forecast-table");
 
-    var minColumn = document.createElement("td");
-    var minTextNode = document.createTextNode(low);
-    line.appendChild(minColumn);
-    minColumn.appendChild(minTextNode);
+        var citiesName = ['Rio de Janeiro', 'São Paulo', 'Belo Horizonte', 'Brasilia', 
+        'Belém', 'Salvador', 'Curitiba', 'Fortaleza', 'Manaus', 'João Pessoa'];
 
-})
-.catch(function (error) {
-    console.log("error");
-});
+        var line = document.createElement("tr");
+        forecastTable.appendChild(line);
+
+        var maxColumn = document.createElement("td");
+        maxColumn.setAttribute("class", "gmr-column-table");
+        var maxTextNode = document.createTextNode(high);
+        line.appendChild(maxColumn);
+        maxColumn.appendChild(maxTextNode);
+
+        var minColumn = document.createElement("td");
+        minColumn.setAttribute("class", "gmr-column-table");
+        var minTextNode = document.createTextNode(low);
+        line.appendChild(minColumn);
+        minColumn.appendChild(minTextNode);
+        
+        var citiesTextNode = document.createTextNode(citiesName[indiceName]);
+        var citiesColumn = document.createElement("td");
+        citiesColumn.setAttribute("class", "gmr-column-table");
+        line.appendChild(citiesColumn);
+        citiesColumn.appendChild(citiesTextNode);    
+        indiceName++;   
+           
+    })
+
+    .catch(function (error) {
+        console.log("error");
+    });
+
+    
+
+}
